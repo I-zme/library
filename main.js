5ea85ee19f32da03a24f3ff71ae97f888002600c
I-zme <inbar.amir21@gmail.com>
2022-12-23 10:00:07 +0200

    Add book button functionality + events
    JS:
    - book button event listeners are created when a new book is added
    - clicking on edit opens the book form with the current info inside, changing and submitting again changes the info in the library and displays the newly submitted details.
    - clicking on del deletes the book from the DOM tree BUT currently not from the library. FIXME
    - clicking on readability changes the display (and class) between the different reading status, BUT currently doesn't update the library. FIXME
    - the library is updated on load using an IIFE. MAKE SURE THIS ACTUALLY WORKS. this potentially enables personal libraries to be kept under log in. (will not be implemented though)
    - the form is not properly submitted, as such also doesn't undergo form validation. FIXME
    - clicking outside the bookform closes it, if it in edit mode the edit mode is cancelled

--- main.js
+++ main.js
@@ -1,38 +1,119 @@
-let myLibrary = [];
+let readBtns = document.querySelectorAll('.readability');
+let delBtns = document.querySelectorAll('.delete');
+let editBtns = document.querySelectorAll('.edit');
 
-function Book(title, author, pages, read) {
+let myLibrary = [];
+let myBooksIndex = 0;
+
+const addBookBtn = document.querySelector('#add-book');
+const bookForm = document.querySelector('.book-form-container');
+const booksContainer = document.querySelector('.book-container');
+const readDict = {
+  read: 'read',
+  reading: 'reading',
+  'not-read': 'not read',
+};
+
+(function () {
+  myLibrary.forEach((book) => {
+    let newBook = createBookHTML(book);
+    addBookListeners(newBook);
+  });
+})();
+
+function Book(title, author, pages, readability) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.readability = readability;
+  this.serial = `bk-${myBooksIndex}`;
 }
+
+// event listeners to pop up the book form for adding books and for closing it on clicking outside
+addBookBtn.addEventListener('click', () => {
+  bookForm.toggleAttribute('data-visible');
+});
+
+document.addEventListener('click', (e) => {
+  if (bookForm.hasAttribute('data-visible')) {
+    if (
+      e.target.closest('.book-form button[type="submit"]') ||
+      !(
+        e.target.closest('.book-form-container') ||
+        e.target.closest('.add-book') ||
+        e.target.closest('.edit')
+      )
+    ) {
+      if (bookForm.hasAttribute('data-edit')) {
+        bookForm.removeAttribute('data-edit');
+      }
+      document.querySelector('.book-form').reset();
+      bookForm.toggleAttribute('data-visible');
+    }
+  }
+});
+
+// adding book
+document
+  .querySelector('.book-form button[type="submit"]')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+    if (bookForm.hasAttribute('data-edit')) {
+      let id = bookForm.getAttribute('data-edit');
+      let bookDiv = document.getElementById(id);
+      const editBook = myLibrary.find((dict) => dict.serial === id);
+      enterInputDetailsInLibrary(editBook);
+      enterBookDetailsInHTML(bookDiv, editBook);
+      bookForm.removeAttribute('data-edit');
+    } else {
+      addBookToLibrary();
+    }
+  });
 
 function addBookToLibrary() {
-  // do stuff here
+  const newBook = new Book();
+  enterInputDetailsInLibrary(newBook);
+  myBooksIndex += 1;
+  myLibrary.push(newBook);
+  createBookHTML(newBook);
 }
 
-const booksContainer = document.querySelector('.book-container');
+function enterInputDetailsInLibrary(book) {
+  book.title = document.querySelector('.book-form input#bk-title').value;
+  book.author = document.querySelector('.book-form input#bk-author').value;
+  book.pages = document.querySelector('.book-form input#bk-pages').value;
+  book.readability = document.querySelector(
+    '.book-form select#bk-readability'
+  ).value;
+}
 
-function createBookHTML({ title, author, pages, read }) {
-  const readDict = {
-    read: 'read',
-    reading: 'reading',
-    'not-read': 'not read',
-  };
+function enterBookDetailsInHTML(htmlBookDiv, libraryBook) {
+  console.log(htmlBookDiv.querySelector('.title'), libraryBook.title);
+  htmlBookDiv.querySelector('.title').textContent = libraryBook.title;
+  htmlBookDiv.querySelector('.author').textContent = libraryBook.author;
+  htmlBookDiv.querySelector(
+    '.pages'
+  ).textContent = `${libraryBook.pages} pages`;
+  htmlBookDiv.querySelector('.readability').textContent =
+    readDict[libraryBook.readability];
+  htmlBookDiv
+    .querySelector('.readability')
+    .classList.add(libraryBook.readability);
+}
+
+// function to create the html of a book div with all its information
+function createBookHTML(book) {
   const bookWrapper = document.createElement('div');
   bookWrapper.classList.add('book');
+  bookWrapper.setAttribute('id', book.serial);
   const titlePara = document.createElement('para');
   titlePara.classList.add('title');
-  titlePara.textContent = title;
   const authorPara = document.createElement('para');
   authorPara.classList.add('author');
-  authorPara.textContent = author;
   const pagesPara = document.createElement('para');
   pagesPara.classList.add('pages');
-  pagesPara.textContent = `${pages} pages`;
   const readBtn = document.createElement('button');
-  readBtn.classList.add('readability', read);
-  readBtn.textContent = readDict[read];
+  readBtn.classList.add('readability');
   const delBtn = document.createElement('button');
   delBtn.classList.add('delete');
   delBtn.textContent = 'Delete';
@@ -49,29 +130,68 @@ function createBookHTML({ title, author, pages, read }) {
     editBtn
   );
   booksContainer.append(bookWrapper);
+  enterBookDetailsInHTML(bookWrapper, book);
+
+  return bookWrapper;
 }
 
-// event listeners for book
-const readBtns = document.querySelectorAll('.readability');
-readBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    const btnClass = btn.classList[btn.classList.length - 1];
-    switch (btnClass) {
-      case 'read':
-        btn.textContent = 'not read';
-        btn.classList.add('not-read');
-        break;
-      case 'not-read':
-        btn.textContent = 'reading';
-        btn.classList.add('reading');
-        break;
-      case 'reading':
-        btn.textContent = 'read';
-        btn.classList.add('read');
-        break;
-    }
-    btn.classList.remove(btnClass);
-  });
+// DOM change event listeners
+booksContainer.addEventListener('DOMNodeInserted', (e) => {
+  if (e.relatedNode.classList.contains('book-container')) {
+    addBookListeners(e.target);
+  }
 });
+
+// book buttons event listeners
+function readToggle(btn) {
+  const btnClass = btn.classList[btn.classList.length - 1];
+  switch (btnClass) {
+    case 'read':
+      btn.textContent = 'not read';
+      btn.classList.add('not-read');
+      break;
+    case 'not-read':
+      btn.textContent = 'reading';
+      btn.classList.add('reading');
+      break;
+    case 'reading':
+      btn.textContent = 'read';
+      btn.classList.add('read');
+      break;
+  }
+  btn.classList.remove(btnClass);
+}
+
+function delToggle(book) {
+  booksContainer.removeChild(book);
+}
+
+function editToggle(book) {
+  bookForm.toggleAttribute('data-visible');
+  bookForm.setAttribute('data-edit', book.getAttribute('id'));
+  const editBook = myLibrary.find(
+    (dict) => dict.serial === book.getAttribute('id')
+  );
+  document.querySelector('.book-form input#bk-title').value = editBook.title;
+  document.querySelector('.book-form input#bk-author').value = editBook.author;
+  document.querySelector('.book-form input#bk-pages').value = editBook.pages;
+  document.querySelector('.book-form select#bk-readability').value =
+    editBook.readability;
+}
+
+function addBookListeners(book) {
+  let btns = book.querySelectorAll('button');
+  btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      if (btn.classList.contains('delete')) {
+        delToggle(book);
+      } else if (btn.classList.contains('edit')) {
+        editToggle(book);
+      } else {
+        readToggle(btn);
+      }
+    });
+  });
+}
 
 // info container - dynamically enter number of books and their readability
